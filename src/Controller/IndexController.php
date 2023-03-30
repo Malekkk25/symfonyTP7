9e8304488cbdf821a0ffcaf9bf462dd0bc1606a3
Your Name <you@example.com>
2023-03-30 23:00:23 +0100

    first commit

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,14 +1,105 @@
 <?php
 namespace App\Controller;
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Form\ArticleType;
+
 class IndexController extends AbstractController
 {
- public function home()
- {
-    $articles=['Article1', 'Article 2', 'Article 3'];
+   private $entityManager;
 
- return $this->render('articles/index.html.twig',['articles' => $articles]);
- 
+   public function __construct(ManagerRegistry $entityManager)
+   {
+       $this->entityManager = $entityManager;
+   }
+   public function home()
+ {
+$articles= $this->entityManager->getRepository(Article::class)->findAll();
+return $this->render('articles/index.html.twig',['articles'=> $articles]);
+
+   
  }
+  
+
+ public function save() {
+   $entityManager = $this->entityManager->getManager();
+   $article = new Article();
+   $article->setNom('Article 1');
+   $article->setPrix(1000);
+   
+   $entityManager->persist($article);
+   $entityManager->flush();
+   return new Response('Article enregisté avec id '.$article->getId());
+   }
+  
+   public function new(Request $request) {
+      $article = new Article();
+      $form = $this->createFormBuilder($article)
+      ->add('nom', TextType::class)
+      ->add('prix', TextType::class)
+      ->add('save', SubmitType::class, array('label' => 'Créer')
+      )->getForm();
+      
+      
+      $form->handleRequest($request);
+      
+      if($form->isSubmitted() && $form->isValid()) {
+      $article = $form->getData();
+      
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($article);
+      $entityManager->flush();
+      
+      return $this->redirectToRoute('article_list');
+      }
+      return $this->render('articles/new.html.twig',['form' => $form->createView()]);
+      }
+public function show($id) {
+         $article = $this->entityManager->getRepository(Article::class)->find($id);
+         return $this->render('articles/show.html.twig', array('article' => $article));
+              }
+
+              public function edit(Request $request, $id) {
+               $article = new Article();
+               $article = $this->entityManager->getRepository(Article::class)->find($id);
+               
+               $form = $this->createFormBuilder($article)
+               ->add('nom', TextType::class)
+               ->add('prix', TextType::class)
+               ->add('save', SubmitType::class, array(
+               'label' => 'Modifier' 
+               ))->getForm();
+               
+               $form->handleRequest($request);
+               if($form->isSubmitted() && $form->isValid()) {
+               
+               $entityManager = $this->entityManager->getManager();
+               $entityManager->flush();
+               
+               return $this->redirectToRoute('article_list');
+               }
+               return $this->render('articles/edit.html.twig', ['form' => $form->createView()]);
+                }
+
+                public function delete(Request $request, $id) {
+                  $article = $this->entityManager->getRepository(Article::class)->find($id);
+                  
+                  $entityManager = $this->entityManager->getManager();
+                  $entityManager->remove($article);
+                  $entityManager->flush();
+                  
+                  $response = new Response();
+                  $response->send();
+                  return $this->redirectToRoute('article_list');
+                  }    
+                                        
+     
 }
